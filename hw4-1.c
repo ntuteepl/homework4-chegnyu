87a24bd7bc9953caf5545d7fdd99cbcbdd8a78dd
chegnyu <146065063+chegnyu@users.noreply.github.com>
2023-10-30 23:22:18 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,c=2;
+    scanf("%d", &n);
+    int i,a[n];
+
+    for (i=0;i<n;i++){
+
+        scanf("%d", &a[i]);
+
+        for (c = 2;c<a[i];c++){
+            if (a[i]%c == 0){
+
+                printf(" NO ");
+                break;
+            }
+        }
+        if (c == a[i]){
+
+        printf(" YES ");
+        }
+    }
+
+    return 0;
+
+
+}
 
